5f752dec0f861ea9fa5ade5cb15e318ae062b310
Moh. Su'aidi <moheedsuaidi@gmail.com>
2025-03-06 22:30:15 +0700

    Finish JWT Auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,11 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-const authRouter = Router()
+import { signUp, signIn, signOut } from "../controllers/auth.controller.js";
 
-authRouter.post('/sign-up',  (req, res) => {
-    res.send({ title: 'Sign Up' });
-})
+const authRouter = Router();
 
-authRouter.post('/sign-in',  (req, res) => {
-    res.send({ title: 'Sign In' });
-})
-
-authRouter.post('/sign-out',  (req, res) => {
-    res.send({ title: 'Sign Out' });
-})
+authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
