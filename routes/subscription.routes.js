df55c2853b866829b45c2a6f0215d27ab5a47b98
Moh. Su'aidi <moheedsuaidi@gmail.com>
2025-03-07 20:29:34 +0700

    Finish Subscription

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,37 +1,38 @@
-import { Router } from 'express';
+import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
 
-const subscriptionRouter = Router()
+const subscriptionRouter = Router();
 
-subscriptionRouter.get('/', (req, res) => {
-    res.send({ title: 'GET All Subscriptions' });
-})
+subscriptionRouter.get("/", (req, res) => {
+  res.send({ title: "GET All Subscriptions" });
+});
 
-subscriptionRouter.get('/:id', (req, res) => {
-    res.send({ title: 'GET Subscription Details' });
-})
+subscriptionRouter.get("/:id", (req, res) => {
+  res.send({ title: "GET Subscription Details" });
+});
 
-subscriptionRouter.post('/', (req, res) => {
-    res.send({ title: 'CREATE Subscription' });
-})
+subscriptionRouter.post("/", authorize, createSubscription);
 
-subscriptionRouter.put('/:id', (req, res) => {
-    res.send({ title: 'UPDATE Subscription' });
-})
+subscriptionRouter.put("/:id", (req, res) => {
+  res.send({ title: "UPDATE Subscription" });
+});
 
-subscriptionRouter.delete('/:id', (req, res) => {
-    res.send({ title: 'DELETE Subscription' });
-})
+subscriptionRouter.delete("/:id", (req, res) => {
+  res.send({ title: "DELETE Subscription" });
+});
 
-subscriptionRouter.get('/user/:id', (req, res) => {
-    res.send({ title: 'GET All User Subscriptions' });
-})
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
 
-subscriptionRouter.put('/:id/cancel', (req, res) => {
-    res.send({ title: 'CANCEL Subscription' });
-})
+subscriptionRouter.put("/:id/cancel", (req, res) => {
+  res.send({ title: "CANCEL Subscription" });
+});
 
-subscriptionRouter.get('/upcoming-renewals', (req, res) => {
-    res.send({ title: 'GET Upcoming Renewals' });
-})
+subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+  res.send({ title: "GET Upcoming Renewals" });
+});
 
 export default subscriptionRouter;
